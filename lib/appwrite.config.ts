a16dcd4d89de178bb2ed3a02a6e7e3ddb63d5c1d
sanskar jain <sanskarj15@gmail.com>
2024-08-17 13:53:48 +0530

    appwrite configs

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,20 @@
+import * as sdk from "node-appwrite";
+
+const {
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENTS_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+} = process.env;
+
+const client = new sdk.Client();
+
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
+const databases = new sdk.Databases(client);
+const users = new sdk.Users(client);
+const message = new sdk.Messaging(client);
+const storage = new sdk.Storage(client);
