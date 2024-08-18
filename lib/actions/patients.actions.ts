a89bd8d105e3ad9d092b6309d5644b193958f7eb
sanskar jain <sanskarj15@gmail.com>
2024-08-18 20:21:07 +0530

    configuring register page

--- lib/actions/patients.actions.ts
+++ lib/actions/patients.actions.ts
@@ -1,24 +1,111 @@
-import { users } from "../appwrite.config";
-import { ID, Query } from "node-appwrite";
-import { parseStringify } from "../utils";
+"use server";
 
+import { ID, Query } from "node-appwrite";
+import { InputFile } from "node-appwrite/file";
+import { databases, storage, users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENTS_COLLECTION_ID,
+  PROJECT_ID,
+} from "../config";
+
+// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
   try {
-    const newUser = await users.create(
+    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+    const newuser = await users.create(
       ID.unique(),
       user.email,
       user.phone,
       undefined,
       user.name
     );
-    console.log(newUser);
-    return parseStringify(newUser);
+
+    return parseStringify(newuser);
   } catch (error: any) {
+    // Check existing user
     if (error && error?.code === 409) {
-      const documents = await users.list([Query.equal("email", [user.email])]);
-      return documents?.users[0];
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return existingUser.users[0];
     }
-    console.log(error);
-    return error;
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
+
+// GET USER
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+    return parseStringify(user);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the user details:",
+      error
+    );
+  }
+};
+
+// REGISTER PATIENT
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    console.log("1 here");
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    let file;
+    if (identificationDocument) {
+      const inputFile =
+        identificationDocument &&
+        InputFile.fromBuffer(
+          identificationDocument?.get("blobFile") as Blob,
+          identificationDocument?.get("fileName") as string
+        );
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENTS_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id ? file.$id : null,
+        identificationDocumentUrl: file?.$id
+          ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+          : null,
+        ...patient,
+      }
+    );
+
+    console.log(newPatient);
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error("An error occurred while creating a new patient:", error);
+  }
+};
+
+// GET PATIENT
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENTS_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the patient details:",
+      error
+    );
   }
 };
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,11 +1,11 @@
 import * as sdk from "node-appwrite";
-import { API_KEY, ENDPOINT, PROJECT_ID } from "./congif";
+import { API_KEY, ENDPOINT, PROJECT_ID } from "./config";
 
 const client = new sdk.Client();
 
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
+export const storage = new sdk.Storage(client);
 export const databases = new sdk.Databases(client);
 export const users = new sdk.Users(client);
 export const messaging = new sdk.Messaging(client);
-export const storage = new sdk.Storage(client);
